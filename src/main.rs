6ebe06e42fa3a56d5b2d03cc83518976c0d6c959
red2fred2 <red2fred2base@live.com>
2023-01-25 23:01:34 -0600

    Add char encode function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+/**
+ * Encodes a unicode as a number 0 to 25 when the character is a-z|A-Z.
+ * Returns None when the character is not alphabetical.
+ */
+fn encode_char(message: char) -> Option<u8> {
+	match message.to_digit(36) {
+		Some(n) => if n < 10 {
+			None
+		} else {
+			Some(u8::try_from(n).unwrap() - 10)
+		},
+		None => None
+	}
+}
+
 fn main() {
-	println!("Hello, world!");
+	println!("{:?}", encode_char('Z'));
 }
